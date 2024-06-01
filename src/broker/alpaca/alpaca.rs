30b06fe775cf138b7655dfe05db86f7a4ac0eb21
ryqdev <ykshang4119@gmail.com>
2024-06-01 19:50:40 +0800

    auth in alpaca trading

--- src/broker/alpaca/alpaca.rs
+++ src/broker/alpaca/alpaca.rs
@@ -1,32 +1,45 @@
+/// https://docs.alpaca.markets/docs/websocket-streaming
 use url::Url;
 use tungstenite::{connect, Message};
 use serde_json;
+use dotenv::dotenv;
+use std::env;
 
 // const TEST_BASE_URL: &str = "wss://stream.data.alpaca.markets/v2/test";
-const BASE_URL: &str = "wss://stream.data.alpaca.markets/v1beta2/crypto";
+const BASE_URL: &str = "wss://paper-api.alpaca.markets/stream";
 
 pub async fn alpaca_trading() {
     log::info!("trade with alpaca");
 
+    dotenv().ok();
+    let key_id = env::var("KEY_ID").expect("Cannot find password in .env file");
+    let secret = env::var("SECRET").expect("Cannot find password in .env file");
     let (mut socket, _response) = connect(Url::parse(BASE_URL).unwrap()).expect("Can't connect");
 
     log::info!("{:#?}", _response);
 
-    let authorization = r#"{"action": "auth", "key": "{KEY ID}", "secret": "{SECRET KEY}"}"#;
-    let subscribe = r#"{"action":"subscribe","trades":["BTC/USD","ETH/USD"],"quotes":[],"bars":[]}"#;
+
+    let authorization = format!(r#"{{
+        "action": "auth",
+        "key": "{key_id}",
+        "secret": "{secret}"
+    }}"#);
+
+    // expected response: {"stream":"authorization","data":{"action":"authenticate","status":"authorized"}}
+
+    let subscribe = r#"{
+    "action":"listening",
+    "data":{
+        "streams": ["trade_updates"]
+    }
+    }"#;
 
     socket.send(Message::Text(authorization.into())).unwrap();
     socket.send(Message::Text(subscribe.into())).unwrap();
 
     loop {
+        log::info!("in loop");
         let msg = socket.read().expect("Error reading message").to_string();
-        if msg != "" {
-            let msg_vector: Vec<serde_json::Value> = serde_json::from_str(&msg).unwrap();
-            for m in msg_vector {
-                if m["T"] == "t" {
-                    log::info!("{:#?}", &m);
-                }
-            }
-        }
+        log::info!("response: {}", msg);
     }
 }
