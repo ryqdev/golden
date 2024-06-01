8d7abbbad751fbfee7ef6022aecbea5761492acf
ryqdev <ykshang4119@gmail.com>
2024-06-01 16:09:40 +0800

    add command help

--- src/cli.rs
+++ src/cli.rs
@@ -1,22 +1,25 @@
-use crate::cmds::backtest::BackTestCommand;
-use crate::cmds::Command;
-use crate::cmds::live::LiveTradingCommand;
-use crate::cmds::paper::PaperTradingCommand;
+use crate::cmds::{
+    Command,
+    backtest::BackTestCommand,
+    live::LiveTradingCommand,
+    paper::PaperTradingCommand
+};
+use anyhow::Result;
 
-
-pub async fn match_cmds() -> anyhow::Result<()> {
+pub async fn match_cmds() -> Result<()> {
     match clap::Command::new("golden")
         .subcommands(vec![
             BackTestCommand::usage().display_order(1),
             PaperTradingCommand::usage().display_order(2),
             LiveTradingCommand::usage().display_order(3)
         ])
+        .arg_required_else_help(true)
         .get_matches()
         .subcommand()
     {
         Some(("backtest", sub_m)) => Ok(BackTestCommand::handler(sub_m).await?),
         Some(("paper-trading", sub_m)) => Ok(PaperTradingCommand::handler(sub_m).await?),
         Some(("live-trading", sub_m)) => Ok(LiveTradingCommand::handler(sub_m).await?),
-        _ => Err(anyhow::Error::msg("Miss arguments. Please open Makefile to get instructions")),
+        _ => Err(anyhow::Error::msg("Match commands fails")),
     }
 }
--- src/err.rs
+++ src/err.rs
@@ -1,6 +1,6 @@
 
 #[derive(Debug)]
 pub enum Error {
+    MatchError,
     CSVError,
 }
-
